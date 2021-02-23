64a16f82fff29a302941e5abe2cba521cb8ef156
Kingdragonsss <79524192+Kingdragonsss@users.noreply.github.com>
2021-02-23 19:27:54 +0300

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -16,15 +16,15 @@ module.exports = message => {
   let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
-  let cmd;
+  let kayıt2
   if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
+    kayıt2 = client.commands.get(command);
   } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
+    kayıt2 = client.commands.get(client.aliases.get(command));
   }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
+  if (kayıt2) {
+    if (perms < kayıt2.conf.permLevel) return;
+    kayıt2.run(client, message, params, perms);
   }
 
 };
